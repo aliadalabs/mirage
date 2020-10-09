2c0ab5a6a211150198044939bf0c5bc1a9007bae
Juanjo Bazán <jjbazan@gmail.com>
2020-10-09 13:00:25 +0200

    add code-analysis

--- mirage.rb
+++ mirage.rb
@@ -13,3 +13,8 @@ post '/echo' do
   payload = request.body.read
   body payload
 end
+
+post 'code-analysis' do
+  payload = JSON.parse(request.body.read)
+  body "The quality of the code in #{payload['target_url']} is #{['not', 'quite', 'really'].sample} #{['good', 'bad', 'the best', 'awful']}"
+end
